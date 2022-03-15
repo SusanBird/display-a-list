50cb9725695bd652ccc11fab23f3de69e38196f8
SusanBird <susanbird391@gmail.com>
2022-03-15 12:03:24 -0700

    renderCandy add DOMS

--- render-candy.js
+++ render-candy.js
@@ -1,6 +1,15 @@
 export function renderCandy(candyObject) {
+    // <div class="candy" style="background: pink">
+    // <h3>Snickers</h3>
+    // <img src='assets/snickers.png'>
+    // <p>Color: brown</p>
+    // <p>Description: candy bar with chocolate, carmel, and peanuts</p>
     
     const candyEl = document.createElement('div');
+    const nameEl = document.createElement('h3');
+    const imgEl = document.createElement('img');
+    const colorEl = document.createElement('p');
+    const descriptionEl = document.createElement('p');
 
     candyEl.classList.add('candy');
 
