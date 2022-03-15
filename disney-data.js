b26fe93e16b0ba697fb36f50892355f975094564
SusanBird <susanbird391@gmail.com>
2022-03-15 14:40:34 -0700

    import disney array

---
+++ disney-data.js
@@ -0,0 +1,34 @@
+export const disneyArr = [
+    {
+        title: 'Turning Red',
+        releaseDate: 'March 11, 2022',
+        finances: {
+            budget: '$175 million',
+            boxOffice: '$4 million'
+        }
+    },
+    {
+        title: 'Encanto',
+        releaseDate: 'November 3, 2021',
+        finances: {
+            budget: '$120 - $150 million',
+            boxOffice: '$250.2 million'
+        }
+    },
+    {
+        title: 'Soul',
+        releaseDate: 'December 25, 2020',
+        finances: {
+            budget: '$150 million',
+            boxOffice: '$121 million'
+        }
+    },
+    {
+        title: 'Raya and the Last Dragon',
+        releaseDate: 'March 5, 2021',
+        finances: {
+            budget: '$100 million',
+            boxOffice: '$130.4 million'
+        }
+    },
+];
